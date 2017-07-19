0ec76d5a11301a2d3375586496bc283b008a7c9a
d-kang <david.kang714@gmail.com>
2017-07-19 14:32:42 -0700

    Add Click Event to Items

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 // TODO
 class App extends React.Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div>
         <h1>My Todo List</h1>
-        <GroceryList />
+        <GroceryList onClick={onListItemClick} groceryList={['Paper Towels', 'Cereal']}/>
       </div>
     )
   }
 }
 
+var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
 
-
-const GroceryList = () => {
+const GroceryList = (props) => {
   return (
     <div>
       <ul>
-        <li>Paper Towels</li>
-        <li>Cereal</li>
+        <li onClick={onListItemClick}>{props.groceryList[0]}</li>
+        <li onClick={onListItemClick}>{props.groceryList[1]}</li>
       </ul>
     </div>
 
